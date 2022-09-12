37627424c2c5f03afe8b06ca3a06b2a4dc352a8d
asakhar <a@sakhar.ru>
2022-09-12 10:26:47 -0700

    changed data format

--- src/command_info.rs
+++ src/command_info.rs
@@ -1,76 +1,20 @@
 use std::{
+  collections::HashMap,
   convert::TryInto,
   error::Error,
   fmt::Display,
   io::{Read, Write},
-  net::{SocketAddr, TcpStream},
+  net::{IpAddr, SocketAddr, TcpStream},
   sync::{mpsc, Mutex},
 };
 
-#[derive(Debug, Clone)]
-enum CommandInfo {
-  Next,
-  Prev,
-  Pause,
-  Vol(f64),
-}
-
-impl TryFrom<[u8; 16]> for CommandInfo {
-  type Error = ClientIdentError;
-  fn try_from(buf: [u8; 16]) -> Result<Self, ClientIdentError> {
-    Ok(match buf {
-      [1, 0, 0, 0, 0, 0, 0, 0, ..] => Self::Next,
-      [2, 0, 0, 0, 0, 0, 0, 0, ..] => Self::Prev,
-      [3, 0, 0, 0, 0, 0, 0, 0, ..] => Self::Pause,
-      [4, 0, 0, 0, 0, 0, 0, 0, ..] => Self::Vol(f64::from_le_bytes(buf[8..16].try_into().unwrap())),
-      _ => return Err(ClientIdentError::InvalidByte),
-    })
-  }
-}
-
-impl From<CommandInfo> for [u8; 16] {
-  fn from(command: CommandInfo) -> Self {
-    let mut buffer = [0u8; 16];
-    buffer[0] = match command {
-      CommandInfo::Next => 1,
-      CommandInfo::Prev => 2,
-      CommandInfo::Pause => 3,
-      CommandInfo::Vol(n) => {
-        buffer[8..16].clone_from_slice(&n.to_le_bytes());
-        4
-      }
-    };
-    buffer
-  }
-}
+use crate::utils::ReadSizedExt;
 
 #[derive(Debug, Clone, Copy)]
 enum ClientType {
   Client,
   Host,
 }
-
-#[derive(Debug, Clone, Copy)]
-enum ClientIdentError {
-  InvalidByte,
-  AlreadyExists,
-  HostNotConnected,
-  FailedToWrite,
-}
-
-impl Display for ClientIdentError {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    f.write_str(match self {
-      ClientIdentError::InvalidByte => "Invalid byte received for client type",
-      ClientIdentError::AlreadyExists => "Host with different address already exists",
-      ClientIdentError::HostNotConnected => "Host is not connected yet",
-      ClientIdentError::FailedToWrite => "Failed to write to host",
-    })
-  }
-}
-
-impl Error for ClientIdentError {}
-
 impl TryFrom<u8> for ClientType {
   type Error = ClientIdentError;
   fn try_from(buf: u8) -> Result<Self, ClientIdentError> {
@@ -82,9 +26,64 @@ impl TryFrom<u8> for ClientType {
   }
 }
 
-static HOST_CHANNEL: Mutex<Option<mpsc::Sender<CommandInfo>>> = Mutex::new(None);
+#[derive(Debug, Clone, Copy)]
+enum ClientIdentError {
+  InvalidByte,
+  HostNotConnected,
+  FailedToWrite,
+}
 
-pub fn handle(client: (TcpStream, SocketAddr)) -> Result<(), Box<dyn Error>> {
+impl Display for ClientIdentError {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.write_str(match self {
+      ClientIdentError::InvalidByte => "Invalid byte received for client type",
+      ClientIdentError::HostNotConnected => "Host is not connected yet",
+      ClientIdentError::FailedToWrite => "Failed to write to host",
+    })
+  }
+}
+
+impl Error for ClientIdentError {}
+
+#[derive(Debug)]
+pub struct DataBlock {
+  #[allow(dead_code)]
+  client: IpAddr,
+  message: Vec<u8>,
+}
+
+#[derive(Debug)]
+pub struct HostChannelInner {
+  sender: Mutex<mpsc::Sender<DataBlock>>,
+  receiver: Mutex<mpsc::Receiver<DataBlock>>,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct HostChannel {
+  inner: *const HostChannelInner,
+}
+
+unsafe impl Send for HostChannel {}
+
+impl HostChannel {
+  pub fn new() -> Self {
+    let (sender, receiver) = mpsc::channel();
+    let receiver = Mutex::new(receiver);
+    let sender = Mutex::new(sender);
+    Self {
+      inner: Box::into_raw(Box::new(HostChannelInner { sender, receiver })),
+    }
+  }
+
+  pub fn deref(&self) -> &'static HostChannelInner {
+    unsafe { &*self.inner }
+  }
+}
+
+pub fn handle(
+  client: (TcpStream, SocketAddr),
+  hosts: &Mutex<HashMap<String, HostChannel>>,
+) -> Result<(), Box<dyn Error>> {
   let (mut stream, addr) = client;
   log::info!("Client {} tries to connect", addr);
   let mut buffer = [0u8; 1];
@@ -93,54 +92,82 @@ pub fn handle(client: (TcpStream, SocketAddr)) -> Result<(), Box<dyn Error>> {
   log::info!("Client pretended to be {requested_type:?}");
   match requested_type {
     ClientType::Host => {
-      if HOST_CHANNEL.lock().expect("Mutex got poisoned").is_some() {
-        stream.write_all(&[0u8; 1])?;
-        Err(ClientIdentError::AlreadyExists)?;
-      }
+      let host_name = String::from_utf8(stream.read_sized()?)?;
+      eprintln!("Pretended hostname: {host_name:?}");
+
+      let mut hosts_lock = hosts.lock().expect("Got poisoned");
+
+      let host_channel = hosts_lock
+        .entry(host_name)
+        .or_insert_with(HostChannel::new)
+        .deref();
+
+      drop(hosts_lock);
       stream.write_all(&[1u8; 1])?;
-      let res = handle_host(stream);
-      *HOST_CHANNEL.lock().expect("Mutex got poisoned") = None;
-      if res.is_err() {
-        return res;
+      let res = handle_host(stream, &host_channel);
+      log::info!("Closed connection to host: {}", client.1);
+      res
+    }
+    ClientType::Client => {
+      let host_name = String::from_utf8(stream.read_sized()?)?;
+      eprintln!("Requested hostname: {host_name:?}");
+      let hosts_lock = hosts.lock().expect("Got poisoned");
+      if let Some(host_channel) = hosts_lock.get(&host_name) {
+        let host_channel = host_channel.deref();
+        drop(hosts_lock);
+        stream.write_all(&[1u8; 1])?;
+        handle_client(stream, client.1.ip(), &*host_channel)
+      } else {
+        stream.write_all(&[0u8; 1])?;
+        Err(Box::new(ClientIdentError::HostNotConnected))
       }
     }
-    ClientType::Client => match *HOST_CHANNEL.lock().expect("Mutex got poisoned") {
-      Some(ref sender) => {
-        stream.write_all(&[1u8; 1])?;
-        log::info!("Ready written to client");
-        handle_client(stream, sender.clone())?;
-      }
-      None => {
-        stream.write_all(&[0u8; 1])?;
-        Err(ClientIdentError::HostNotConnected)?;
-      }
-    },
   }
-  Ok(())
 }
 
-fn handle_host(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
-  let (sender, receiver) = mpsc::channel();
-  *HOST_CHANNEL.lock().expect("Mutex got poisoned") = Some(sender);
+fn handle_host(
+  mut stream: TcpStream,
+  host_channel: &HostChannelInner,
+) -> Result<(), Box<dyn Error>> {
   let mut buf = [0u8; 1];
-
-  while stream.read_exact(&mut buf).is_ok() && buf[0] != 2 {
+  let receiver = match host_channel.receiver.try_lock() {
+    Ok(recv) => recv,
+    Err(std::sync::TryLockError::WouldBlock) => {
+      return Err(Box::new(
+        std::sync::TryLockError::<mpsc::Receiver<DataBlock>>::WouldBlock,
+      ))
+    }
+    _ => unreachable!("Got poisoned"),
+  };
+  'outer: while stream.read_exact(&mut buf).is_ok() && buf[0] != 2 {
     match buf[0] {
       0 => continue,
       1 => {}
       _ => return Err(Box::new(ClientIdentError::InvalidByte)),
     }
 
-    let command: [u8; 16] = match receiver.try_recv() {
-      Ok(command) => command.into(),
-      Err(mpsc::TryRecvError::Empty) => {
-        stream.write_all(&[0])?;
-        continue;
+    let fetch_start = std::time::SystemTime::now();
+    let data_block = loop {
+      let res = receiver.try_recv();
+      match res {
+        Ok(db) => break db,
+        Err(mpsc::TryRecvError::Empty) => {
+          if std::time::SystemTime::now()
+            .duration_since(fetch_start)
+            .expect("fetch_start was later then now()")
+            .as_millis()
+            > 100
+          {
+            stream.write_all(&[0])?;
+            continue 'outer;
+          }
+        }
+        _ => unreachable!("Sender disconnected"),
       }
-      _ => unreachable!("Sender disconnected"),
     };
-    stream.write_all(&[1])?;
-    if stream.write_all(&command).is_err() {
+    log::info!("Data block is being sent to host: {data_block:?}");
+    stream.write_all(&[data_block.message.len() as u8])?;
+    if stream.write_all(&data_block.message).is_err() {
       return Err(Box::new(ClientIdentError::FailedToWrite));
     }
   }
@@ -150,12 +177,12 @@ fn handle_host(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
 
 fn handle_client(
   mut stream: TcpStream,
-  sender: mpsc::Sender<CommandInfo>,
+  client: IpAddr,
+  host_channel: &HostChannelInner,
 ) -> Result<(), Box<dyn Error>> {
-  let mut buffer = [0u8; 16];
-  stream.read_exact(&mut buffer)?;
-  let command = CommandInfo::try_from(buffer)?;
-  log::info!("Command read from client: {command:?}");
-  sender.send(command)?;
+  let message = stream.read_sized()?;
+  let sender = host_channel.sender.lock().expect("Got poisoned").clone();
+  log::info!("Message read from client({client}): {message:?}");
+  sender.send(DataBlock { message, client })?;
   Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
 use std::{
+  collections::HashMap,
   error::Error,
   net::{SocketAddr, TcpListener},
+  sync::Mutex,
 };
 
 mod command_info;
+mod utils;
 
 fn main() -> Result<(), Box<dyn Error>> {
   dotenv::dotenv()?;
   env_logger::init();
   let server_addr: SocketAddr = std::env::var("COMMAND_SERVER_ADDRESS")?.parse()?;
   let listener = TcpListener::bind(server_addr)?;
+  let hosts = Mutex::new(HashMap::new());
   log::info!("Server is running on {}", server_addr);
-  loop {
+  std::thread::scope(|scope| loop {
     for client in listener.accept() {
-      std::thread::spawn(move || {
-        if let Err(why) = command_info::handle(client) {
+      let hosts = &hosts;
+      scope.spawn(move || {
+        if let Err(why) = command_info::handle(client, hosts) {
           log::error!("{}", why);
         }
       });
     }
-  }
+  });
+  unreachable!()
 }
